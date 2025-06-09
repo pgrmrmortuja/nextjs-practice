f6bcf82d544be33acae720d90f145cb3b3078cc1
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-06-10 00:43:47 +0600

    use image in mission page

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,11 +1,14 @@
 import Button from '@/app/components/Button';
 import React from 'react';
+import nextimage from '@/public/next.svg';
+import Image from 'next/image';
 
 const MissionPage = () => {
     return (
         <div>
             <h1>This is Mission Page</h1>
             <Button></Button>
+            <Image src={nextimage} alt='next js logo image' quality={100}></Image>
         </div>
     );
 };
