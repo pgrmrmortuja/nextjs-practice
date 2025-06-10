1c28338366c16721aa27f0348a6d4a7279cc251a
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-06-10 15:49:57 +0600

    parallal requests accept

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,4 +1,5 @@
 import getPost from '@/lib/getPost';
+import getPostComment from '@/lib/getPostComment';
 import React from 'react'
 
 export async function generateMetadata({ params }) {
@@ -15,12 +16,30 @@ export async function generateMetadata({ params }) {
 export default async function PostPage({ params }) {
     const { id } = params;
 
-    const post = await getPost(id);
+    const postPromise = getPost(id);
+    const commentsPromise = getPostComment(id);
+
+    const [post, comments] = await Promise.all([postPromise, commentsPromise]);
+
     console.log(post.id);
 
     return (
         <div>
             <h2>{post.title}</h2>
+
+            <hr></hr>
+            <div className='mt-5'>
+                <h2>Post Comments:</h2>
+
+                <ul>
+                    {
+                        comments.map((comment) =>(
+                            <li className='mb-2' key={comment.id}>{comment.body}</li>
+                        ))
+                    }
+                </ul>
+
+            </div>
         </div>
     )
 }
