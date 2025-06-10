9a6821023170b235eaf586a9cad1a6dd58779ff9
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-06-10 13:38:59 +0600

    fetcing all posts

---
+++ app/posts/page.js
@@ -0,0 +1,23 @@
+import getAllPosts from '@/lib/getAllPosts'
+import Link from 'next/link';
+import React from 'react'
+
+export default async function Posts() {
+
+  const posts = await getAllPosts();
+  console.log(posts);
+
+  return (
+    <div className='mt-6'>
+      <h1>All Posts</h1>
+
+      <ul>
+        {
+          posts.map((post) => (
+            <li key={post.id}><Link href={`/posts/${post.id}`}>{post.title}</Link></li>
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
