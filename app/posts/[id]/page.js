4e6da949d0a23ee8f04cd2d36cbf2ce8d8010aa2
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-06-10 14:04:33 +0600

    generateMetadata for each post

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,15 +1,26 @@
 import getPost from '@/lib/getPost';
 import React from 'react'
 
-export default async function PostPage({params}) {
-    const {id} = params;
+export async function generateMetadata({ params }) {
+
+    const { id } = params;
+    const post = await getPost(id);
+
+    return {
+        title: post.title,
+        description: post.body,
+    }
+};
+
+export default async function PostPage({ params }) {
+    const { id } = params;
 
     const post = await getPost(id);
     console.log(post.id);
 
-  return (
-    <div>
-        <h2>{post.title}</h2>
-    </div>
-  )
+    return (
+        <div>
+            <h2>{post.title}</h2>
+        </div>
+    )
 }
